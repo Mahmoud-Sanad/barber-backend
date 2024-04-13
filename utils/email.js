1a62e3ec23ba31922c8f95c5b3b28d8ad095180d
Mahmoud-Sanad <82536865+Mahmoud-Sanad@users.noreply.github.com>
2024-04-13 20:39:55 +0200

    fg

--- utils/email.js
+++ utils/email.js
@@ -4,13 +4,13 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         service: "hotmail",
         auth: {
-            user: "yourhotmail@email.com",
-            pass: "yourpassword",
+            user: "salon.barber@outlook.com",
+            pass: "123Barber",
         },
     });
 
     const mailOptions = {
-        from: '"Your Name" <yourhotmail@email.com>', 
+        from: 'Salon <salon.barber@outlook.com>', 
         to: options.email, 
         subject: options.subject, 
         text: options.message, 
