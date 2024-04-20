b02cb38940ef2fe686279e27daed67fd5268af67
Mahmoud-Sanad <82536865+Mahmoud-Sanad@users.noreply.github.com>
2024-04-20 15:49:59 +0200

    ff

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,5 +2,6 @@ const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
 const router=require("express").Router();
 router.use(authController.isLoggedIn);
-router.route("/booking").get(userController.getMyBooking)
+router.route("/booking").get(userController.getMyBooking);
+router.route("/booking/:id").get(userController.getBookingById);
 module.exports=router;
