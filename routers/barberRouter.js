486d1c95d2ea6fd14660d951688e6e8024f6986c
Mahmoud-Sanad <82536865+Mahmoud-Sanad@users.noreply.github.com>
2024-05-09 16:14:19 +0300

    g

--- routers/barberRouter.js
+++ routers/barberRouter.js
@@ -2,7 +2,7 @@ const authController = require("../controllers/authController");
 const barberController = require("../controllers/barberController");
 const router=require("express").Router();
 router.use(authController.isLoggedIn);
-router.route("/").post(authController.restrictTo("Barber"),barberController.createStore).get(barberController.getAllBarbers);
+router.route("/").post(authController.restrictTo("Barber"),barberController.createServices,barberController.createStore).get(barberController.getAllBarbers);
 router.route('/services').get(barberController.getAllServices);
 router.route('/bookings/status/:id').post(barberController.changeBookingStatus);
 router.route('/bookings/:id').get(barberController.getStoreBookings);
